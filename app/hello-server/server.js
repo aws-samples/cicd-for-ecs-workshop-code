b2c1bf9ba42c2474e739f51398a6786ce4b54bd0
Mike Rizzo <michariz@amazon.co.uk>
2021-04-07 16:26:32 +0100

    Tidied hello-server

--- app/hello-server/server.js
+++ app/hello-server/server.js
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-'use strict';
+'use strict'
 
 const greeting = "Hi there"  // Change this line to change your greeting
 
@@ -9,12 +9,12 @@ const port = (typeof process.env.PORT !== 'undefined')
   ? process.env.PORT
   : '80'
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Constants
 const host = '0.0.0.0';
@@ -27,15 +27,15 @@ function format(o, pretty) {
 
 app.get('/hello/:name', (req, res) => {
   var name = req.params.name
-  let timestamp = Date.now();
+  let timestamp = Date.now()
   res.send(greeting + " " + name + "<br>\n<i>" + timestamp + "</i>\n") 
-});
+})
 app.get('/ping', (req, res) => {
-  res.send("ok");
-});
+  res.send("ok")
+})
 
-const server = app.listen(port, HOST);
-console.log(`Service running on http://${host}:${port}`);
+const server = app.listen(port, host);
+console.log(`Service running on http://${host}:${port}`)
 
 process.on('SIGTERM', () => {
   console.info('SIGTERM signal received.');
@@ -43,5 +43,5 @@ process.on('SIGTERM', () => {
   server.close(() => {
     console.log('Http server closed.');
     process.exit(0);
-  });
-});
+  })
+})
